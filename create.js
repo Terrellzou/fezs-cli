81e91f567219cae64e7d737240d0a33be36ed21f
Shay <838481064@qq.com>
2023-04-19 00:18:46 +0800

    fix: 拷贝模板

--- create.js
+++ create.js
@@ -4,36 +4,58 @@ const inquirer = require("inquirer");
 const chalk = require("chalk");
 const download = require("download-git-repo");
 const ora = require("ora");
+const { resolve } = require("path");
 
 module.exports = async function (name, options) {
   const cwd = process.cwd();
   // 创建项目的地址
   const targetFile = path.join(cwd, name);
 
-  const downLoadTemplate = (config) => {
-    const { variant } = config;
-    const message = "loading template";
-    const spinner = ora(message);
-    return new Promise((resolve, reject) => {
-      // 1: ts 2: js
-      const downLoadUrl = {
-        1: "direct:https://github.com/Terrellzou/fezs-cli.git",
-        2: "direct:https://github.com/Terrellzou/fe-cli/tree/main/template/template-react",
-      };
-      spinner.start();
-      download(downLoadUrl[variant], targetFile, { clone: false }, (err) => {
-        if (err) {
-          console.log(err);
-          spinner.stop();
-          console.log(chalk.redBright("找不到模板地址"));
-        } else {
-          spinner.stop();
-          spinner.succeed("Loading succeed");
-          resolve(targetFile);
+  const isExist = (path) => {
+    // 判断文件夹是否存在, 不存在创建一个
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+  };
+
+  const copyFile = (sourcePath, targetPath) => {
+    return new Promise(() => {
+      const sourceFile = fs.readdirSync(sourcePath, { withFileTypes: true });
+
+      sourceFile.forEach((file) => {
+        const newSourcePath = path.resolve(sourcePath, file.name);
+        const newTargetPath = path.resolve(targetPath, file.name);
+        if (file.isDirectory()) {
+          isExist(newTargetPath);
+          copyFile(newSourcePath, newTargetPath);
         }
+        fs.copyFileSync(newSourcePath, newTargetPath);
       });
     });
   };
+
+  // const downLoadTemplate = (config) => {
+
+  //   return new Promise((resolve, reject) => {
+  //     // 1: ts 2: js
+  //     const downLoadUrl = {
+  //       1: "direct:https://github.com/Terrellzou/fezs-cli.git",
+  //       2: "direct:https://github.com/Terrellzou/fe-cli/tree/main/template/template-react",
+  //     };
+  //     spinner.start();
+  //     download(downLoadUrl[variant], targetFile, { clone: true }, (err) => {
+  //       if (err) {
+  //         console.log(err);
+  //         spinner.stop();
+  //         console.log(chalk.redBright("找不到模板地址"));
+  //       } else {
+  //         spinner.stop();
+  //         spinner.succeed("Loading succeed");
+  //         resolve(targetFile);
+  //       }
+  //     });
+  //   });
+  // };
 
   const createProject = () => {
     const questions = [
@@ -72,17 +94,28 @@ module.exports = async function (name, options) {
       .prompt(questions)
       .then((answers) => {
         console.log(answers);
-        const { buildTool } = answers;
+        const { buildTool, variant } = answers;
         const buildToolMap = {
           1: "webpack webpack-cli",
-          2: "@vite/cli"
-        }
+          2: "@vite/cli",
+        };
         // 远程下载模板
-        const promise = downLoadTemplate(answers);
-        // 创建构建工具，默认是（vite)
-        // 定义需要按照的依赖
-        const baseDependencies = ["react", buildToolMap[buildTool]];
-        promise.then(() => {
+        // const promise = downLoadTemplate(answers);
+        // 复制模板
+        const message = "loading template";
+        const spinner = ora(message);
+        const copyPath = {
+          1: "template",
+          2: "template/template-react",
+        };
+
+        copyFile(copyPath[variant], 'test').then(() => {
+          // spinner.start();
+          // spinner.stop();
+          // spinner.succeed("Loading succeed");
+          // 创建构建工具，默认是（vite)
+          // 定义需要按照的依赖
+          const baseDependencies = ["react", buildToolMap[buildTool]];
           const child = spawn("pnpm", ["install"].concat(baseDependencies), {
             stdio: "inherit",
           });
@@ -97,6 +130,22 @@ module.exports = async function (name, options) {
             }
           });
         });
+
+        // promise.then(() => {
+        //   const child = spawn("pnpm", ["install"].concat(baseDependencies), {
+        //     stdio: "inherit",
+        //   });
+        //   // 监听执行结果
+        //   child.on("close", function (code) {
+        //     // 执行失败
+        //     if (code !== 0) {
+        //       console.log(
+        //         chalk.red("Error occurred while installing dependencies!")
+        //       );
+        //       process.exit(1);
+        //     }
+        //   });
+        // });
       })
       .catch((e) => {
         console.log(e);
